058d7876d6ea5ba0347a10ec922989547cf6e939
Sigit Dwi Saputro <sigit.dsa3@yahoo.co.id>
2022-10-22 13:43:04 +0700

    [ADD] employee model and migration

---
+++ database/migrations/2022_10_22_063341_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->string('address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
