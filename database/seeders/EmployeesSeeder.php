4a73b9608928fff3a867f7f45cb7bd0c471ce613
Sigit Dwi Saputro <sigit.dsa3@yahoo.co.id>
2022-10-23 16:24:53 +0700

    [UPDATE] create employee seeder data using faker

--- database/seeders/EmployeesSeeder.php
+++ database/seeders/EmployeesSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Employee;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class EmployeesSeeder extends Seeder
 {
@@ -15,23 +16,33 @@ class EmployeesSeeder extends Seeder
      */
     public function run()
     {
-        $employees = [
-            [
-                'name' => 'Name One',
-                'phone_number' => 'Phone One',
-                'email' => 'Email One',
-                'address' => 'Address One'
-            ],
-            [
-                'name' => 'Name Two',
-                'phone_number' => 'Phone Two',
-                'email' => 'Email Two',
-                'address' => 'Address Two'
-            ]
-        ];
+        // $employees = [
+        //     [
+        //         'name' => 'Name One',
+        //         'phone_number' => 'Phone One',
+        //         'email' => 'Email One',
+        //         'address' => 'Address One'
+        //     ],
+        //     [
+        //         'name' => 'Name Two',
+        //         'phone_number' => 'Phone Two',
+        //         'email' => 'Email Two',
+        //         'address' => 'Address Two'
+        //     ]
+        // ];
 
-        foreach ($employees as $key => $value) {
-            Employee::create($value);
+        // foreach ($employees as $key => $value) {
+        //     Employee::create($value);
+        // }
+
+        $faker = Faker::create();
+        foreach (range(1, 100000) as $index) {
+            Employee::create([
+                'name' => $faker->name,
+                'phone_number' => rand(100000000000, 999999999999),
+                'email' => $faker->email,
+                'address' => $faker->address,
+            ]);
         }
     }
 }
